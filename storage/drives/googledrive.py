f9945f04c58e491c592ff539fb18b188db984a38
thawne <thawne2018@gmail.com>
2021-06-25 19:48:20 +0800

    remove something unnecessary

--- storage/drives/googledrive.py
+++ storage/drives/googledrive.py
@@ -1,19 +1,9 @@
 import requests
-
+import json
 
 base_url = 'https://accounts.google.com/o/oauth2/v2/auth'
 auth_url = 'https://accounts.google.com/o/oauth2/v2/auth'
 
-
-client_id = '216440066284-4rc0jvuh02q8hfd5fgev4ris01rqq8ei.apps.googleusercontent.com'
-client_secret = '5RIx-WERt77b2OD_DDXZDBju'
-
-
-proxy = '127.0.0.1:1082'
-proxies = {
-    'http': 'http://' + proxy,
-    'https': 'https://' + proxy,
-}
 
 
 def get_login_code(client_id, redirect_uri):
@@ -25,9 +15,9 @@ def get_login_code(client_id, redirect_uri):
         'access_type': 'offline',
         'state': 'sth that could be converted'
     }
-    response = requests.get(auth_url, data=auth_data, proxies=proxies).json()
+    response = requests.get(auth_url, params=json.dumps(auth_data)).json()
     print(response)
     return response
 
-# get_login_code(client_id, client_secret)
-print(requests.get('https://www.twitter.com', proxies=proxies))
+
+get_login_code(client_id, client_secret)
