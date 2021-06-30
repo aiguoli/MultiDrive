5b5c2d0821f4b528e09925c6933af1e2bffab172
thawne <thawne2018@gmail.com>
2021-06-30 19:33:00 +0800

    :bug:Fix a bug

--- storage/drives/googledrive.py
+++ storage/drives/googledrive.py
@@ -5,7 +5,6 @@ base_url = 'https://accounts.google.com/o/oauth2/v2/auth'
 auth_url = 'https://accounts.google.com/o/oauth2/v2/auth'
 
 
-
 def get_login_code(client_id, redirect_uri):
     auth_data = {
         'client_id': client_id,
